aefe91d44a44d718aaab61866fd3e860a89cd33a
Cardoso Rezende Morais, Douglas <Douglas.CardosoRezendeMorais@windriver.com>
2022-02-21 22:37:48 -0300

    Moving the functions to Calculator file

---
+++ widgets/calculator/src/Calculator.tsx
@@ -0,0 +1,115 @@
+// @ts-nocheck File not migrated fully to TS
+
+import { useState } from 'react';
+import type { FunctionComponent } from 'react';
+import Button from './Button';
+import Screen from './Screen';
+import Header from './Header';
+import ButtonGroup from './ButtonGroup';
+
+
+function CalculatorFunc() {
+
+    const [theValue1, setValue1] = useState(0);
+    const [theValue2, setValue2] = useState(0);
+    const [theResult, setResult] = useState(0);
+    const [theOperation, setOperation] = useState('+')
+
+    const setValues = (value: number) => {
+        if (theValue1 === 0) {
+            setValue1(value)
+        }
+        else {
+            setValue2(value)
+        }
+    }
+
+    const clear = () => {
+        setValue1(0)
+        setValue2(0)
+        setResult(0)
+    };
+
+    const calculate = (operation: string, value1: number, value2: number) => {
+        let result = 0;
+        if (operation === '+') {
+            result = value1 + value2;
+        }
+        if (operation === '*') {
+            result = value1 * value2;
+        }
+        if (operation === '-') {
+            result = value1 - value2;
+        }
+        if (operation === '/') {
+            result = value1 / value2;
+        }
+        setResult(result)
+        return theResult;
+    };
+
+    return (
+        // <div>
+        //     <Screen theValue1={theValue1} theValue2={theValue2} theResult={theResult} theOperation="+" />
+        //     <Button label="1" onClick={() => setValues(1)} />
+        //     <Button label="2" onClick={() => setValues(2)} />
+        //     <Button label="3" onClick={() => setValues(3)} />
+        //     <Button label='+' onClick={() => setOperation('+')} />
+        //     <Button label="Clear" onClick={() => clear()} />
+        //     <Button label="Calculate" onClick={() => calculate(theOperation, theValue1, theValue2)} />
+        // </div>
+        <div className="App">
+      <Header title='This is a simple calculator' />
+      <Screen
+        theValue1={theValue1}
+        theValue2={theValue2}
+        theOperation={theOperation}
+        theResult={theResult} />
+      <ButtonGroup buttons={[
+        <Button label='0' onClick={() => setValues(0)} />,
+        <Button label='1' onClick={() => setValues(1)} />,
+        <Button label='2' onClick={() => setValues(2)} />
+      ]
+      }
+      />
+      <ButtonGroup buttons={[
+        <Button label='3' onClick={() => setValues(3)} />,
+        <Button label='4' onClick={() => setValues(4)} />,
+        <Button label='5' onClick={() => setValues(5)} />
+      ]
+      }
+      />
+      <ButtonGroup buttons={[
+        <Button label='6' onClick={() => setValues(6)} />,
+        <Button label='7' onClick={() => setValues(7)} />,
+        <Button label='8' onClick={() => setValues(8)} />
+      ]
+      }
+      />
+      <ButtonGroup buttons={[
+        <Button label='9' onClick={() => setValues(9)} />,
+        <Button label='+' onClick={() => setOperation('+')} />,
+        <Button label='-' onClick={() => setOperation('-')} />
+      ]
+      }
+      />
+      
+      <ButtonGroup buttons={[
+        <Button label='c' onClick={() => clear()} />,
+        <Button label='*' onClick={() => setOperation('*')} />,
+        <Button label='/' onClick={() => setOperation('/')} />,
+      ]
+      }
+      />
+      <Button label='=' onClick={() => calculate(theOperation, theValue1, theValue2)} />
+    </div>
+    );
+
+    
+}
+
+const Calculator: FunctionComponent = () => {
+    return <CalculatorFunc />
+};
+
+export default Calculator;
--- widgets/calculator/src/widget.tsx
+++ widgets/calculator/src/widget.tsx
@@ -1,47 +1,13 @@
 // @ts-nocheck File not migrated fully to TS
 
-import Button from './Button';
-import Screen from './Screen';
-
-let theValue1 = 0;
-let theValue2 = 0;
-let theResult = 0;
-
-const setValues = (value: number) => {
-    if (theValue1 === 0) theValue1 = value;
-    else theValue2 = value;
-};
-
-const clear = () => {
-    theValue1 = 0;
-    theValue2 = 0;
-    theResult = 0;
-};
-
-const calculate = (operation: string, value1: number, value2: number) => {
-    let result = 0;
-    if (operation === '+') {
-        result = value1 + value2;
-    }
-    if (operation === '*') {
-        result = value1 * value2;
-    }
-    if (operation === '-') {
-        result = value1 - value2;
-    }
-    if (operation === '/') {
-        result = value1 / value2;
-    }
-    theResult = result;
-    return theResult;
-};
+import Calculator from './Calculator';
 
 Stage.defineWidget({
     id: 'calculator',
     name: '@Simple Calculator',
     description: 'This is a sample widget for widget writing boilerplate',
     initialWidth: 12,
-    initialHeight: 8,
+    initialHeight: 20,
     color: 'green',
     isReact: true,
     permission: Stage.GenericConfig.CUSTOM_WIDGET_PERMISSIONS.CUSTOM_ALL,
@@ -49,14 +15,7 @@ Stage.defineWidget({
 
     render(widget, data, error, toolbox) {
         return (
-            <div>
-                <Screen theValue1={theValue1} theValue2={theValue2} theResult={theResult} theOperation="+" />
-                <Button label="1" onClick={() => setValues(1)} />
-                <Button label="2" onClick={() => setValues(2)} />
-                <Button label="3" onClick={() => setValues(3)} />
-                <Button label="Clear" onClick={() => clear()} />
-                <Button label="Calculate" onClick={() => calculate('+', theValue1, theValue2)} />
-            </div>
+            <Calculator />
         );
     }
 });
